3aace50e0a26cda24d4f4878d0b528c4dcb26f2e
cdag16 <40123148@gm.nfu.edu.tw>
2014-06-18 15:28:56 +0800

    change2

--- setup.py
+++ setup.py
@@ -1,18 +1,12 @@
 from setuptools import setup
 
-setup(name='YourAppName',
-      version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-<<<<<<< HEAD
-<<<<<<< HEAD
-      install_requires=['CherryPy'],
-=======
-#      install_requires=['Django>=1.3'],
->>>>>>> fcc7ba9eca64e3297ca5b9c171df4972217ae118
-=======
-#      install_requires=['Django>=1.3'],
->>>>>>> 81bf3da137e92227ede49d25e348e252064832c9
-     )
+setup(
+    name='YourAppName',
+    version='1.0',
+    description='OpenShift App',
+    author='Your Name',
+    author_email='example@example.com',
+    url='http://www.python.org/sigs/distutils-sig/',
+    install_requires=['CherryPy'],
+    # install_requires=['Django>=1.3'],
+)
