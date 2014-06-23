5712eb3f200394a8a37c0e74ed8a4b5dc2041c54
2014cdag14 <40123136@gm.nfu.edu.tw>
2014-06-23 21:58:16 +0800

    '2cdag14'

--- wsgi/programs/cdag14/__init__.py
+++ wsgi/programs/cdag14/__init__.py
@@ -288,7 +288,7 @@ three_plane_assembly(session, assembly, transf, featID, 3, "beam_5.prt", "DTM6",
 three_plane_assembly(session, assembly, transf, featID, 1, "crossblockb.prt", "DTM2", "RIGHT", "FRONT", "DTM6", "DTM5", "FRONT");
 
 //10
-three_plane_assembly(session, assembly, transf, featID, 7, "beam_5.prt", "DTM4", "DTM7", "RIGHT", "DTM4", "DTM8", "FRONT");
+three_plane_assembly(session, assembly, transf, featID, 7, "beam_5.prt", "DTM4", "DTM7", "DTM9", "DTM4", "DTM8", "DTM7");
 
 //11
 //three_plane_assembly(session, assembly, transf, featID, 9, "conn.prt", "DTM9", "RIGHT", "A_2", "DTM2", "RIGHT", "A_1");
